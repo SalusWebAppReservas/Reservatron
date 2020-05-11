0baba282739c21bd0ba3a2d5649c66f033442ac7
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-05-11 20:35:19 +0200

    Añadida configuración extra al servidor. * Añadida extname para que funciona con .hbs * Definida ruta para archivos estáticos. * Definida ruta para vistas * Ruta de prueba definida en userRegistration * Añadida librería path para usar join * Añadidos varios archivos vacios para que se creen lor directorios en github.

--- src/server.js
+++ src/server.js
@@ -1,18 +1,28 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const hbs = require("express-handlebars");
+const app = express();
+const { join } = require("path");
+const port = process.env.PORT || 3000;
 
-app.engine('hbs',hbs({
-    defaultLayout:'home'
-}))
-app.set('view engine','hbs')
+app.engine(
+  "hbs",
+  hbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", join(__dirname, "view"));
 
-app.use(express.urlencoded({
-    extended:false
-}))
+app.use(express.static(join(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
+app.get("/", (req, res) => res.render("userRegistration"));
 
-
-
-app.listen(port,()=>console.log(`APP funcionado en http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`APP funcionado en http://localhost:${port}`)
+);

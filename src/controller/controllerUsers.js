57fa80c041697a3e872ea0545d1f02d2ed94a56c
diegofr-98 <diegoxofr98@gmail.com>
2020-05-12 16:38:39 +0200

    endpoints usuario

---
+++ src/controller/controllerUsers.js
@@ -0,0 +1,11 @@
+const verifyUser = (req, res, next) => {};
+const getUser = (req, res, next) => {};
+const addUser = (req, res, next) => {};
+const modifyUser = (req, res, next) => {};
+
+module.exports = {
+    verifyUser,
+    getUser,
+    addUser,
+    modifyUser,
+};

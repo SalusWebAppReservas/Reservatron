642bab52167de4de0c0e351ccc270b981d9679e7
diegofr-98 <diegoxofr98@gmail.com>
2020-05-13 19:15:56 +0200

    ruta registro

--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const { renderAPP } = require('../controller/controllerMain.js');
 const { getFirebaseConfig } = require('../controller/controllerFirebase.js');
 const { loginUser, getUserData, addUser, modifyUser } = require('../controller/controllerUsers');
-
+const { registro } = require('../controller/controllerRegistro');
 const router = express.Router();
 
 router.get('/', renderAPP);
+router.get('/registro', registro);
 router.get('/getFirebaseConfig', getFirebaseConfig);
 router.get('/loginUser/:user', loginUser);
 router.get('/getUserData/:userID', getUserData);

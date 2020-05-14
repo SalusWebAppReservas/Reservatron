da8174617e8f6c3d733ddc16b5f40eea840c25e9
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-05-14 07:47:27 +0200

    Responsive Design

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,6 +2,14 @@ const login = document.getElementById('mainLogin');
 const register = document.getElementById('mainRegister');
 const contenedor = document.getElementById('contenedor');
 
+const showStepTwo = () => {
+    formPaso1.classList.add('step2');
+};
+
+const showStepThree = () => {
+    formPaso1.classList.add('step2');
+};
+
 const renderLogin = (e) => {
     e.preventDefault();
     contenedor.innerHTML = Handlebars.templates['login.hbs']();
@@ -11,7 +19,12 @@ const renderLogin = (e) => {
 const renderRegister = (e) => {
     e.preventDefault();
     contenedor.innerHTML = Handlebars.templates['userRegistration.hbs']();
+    const btnSiguiente1 = document.getElementById('btnFormulario__siguiente1');
+    const btnSiguiente2 = document.getElementById('btnFormulario__siguiente2');
+    const formPaso1 = document.getElementById('formulario__paso1');
     const btnCreateAccount = document.getElementById('btnCreateAccount');
+    btnSiguiente1.addEventListener('click', () => formPaso1.classList.add('step2'));
+    btnSiguiente2.addEventListener('click', () => formPaso1.classList.add('step3'));
     btnCreateAccount.addEventListener('click', verifyUserBySMS);
 };
 

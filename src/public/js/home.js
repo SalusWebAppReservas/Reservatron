9f991b925b3c0d7513d209f93bb54498f15589fb
JordiGilTomas <gil.tomas.jordi@gmail.com>
2020-05-20 19:57:54 +0200

    Añadido calendario y añadidos patrones al registro * Añadida funcionalidad calendario * Creada función para calcular días de los meses y semana anterior y poseterior para formar el calendario. * Creaada función para mostrar el nombre del día y la fecha * Creada función para mostrar el nombre del mes y del año. * Añadidos patrones, required y titulos al registro

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,25 +1,10 @@
 // Ojo, modificada linea 36 en handlebars.js porque usando imports se usa strict mode
 // added (|| window) to 'this' porque en strict mode 'this' no puede acceder a 'window'.
 import { connectFirebase } from './model/fireBase.js';
-import {
-    homeTemplate,
-    loginTemplate,
-    userRegistrationTemplate,
-    adminShowReservasTemplate,
-    changeIconToLogOut,
-    changeIconToLogIn,
-    registerGoToStep2,
-    registerGoToStep3,
-    registerBackToStep2,
-    registerBackToStep1,
-    changeIconDayMonth,
-    changeIconMonthToDay,
-    adminReservasDay,
-    adminReservasMonth,
-} from './view/UI.js';
 import verifyUserBySMS from './userRegistration.js';
 import { verifyLoginUser, sendLoginUser } from './login.js';
 import { getReservas } from './model/db.js';
+import * as UI from './view/UI.js';
 
 const login = document.getElementById('mainLogin');
 const register = document.getElementById('btnRegister');
@@ -27,32 +12,33 @@ const contenedor = document.getElementById('contenedor');
 const logoHome = document.getElementById('logoHome');
 
 const logout = async () => {
-    await connectFirebase();
-    if (firebase) await firebase.auth().signOut();
+    if (firebase.apps.length !== 0 && firebase) await firebase.auth().signOut();
     sessionStorage.removeItem('RVuserID');
     renderHome();
-    changeIconToLogIn();
+    UI.changeIconToLogIn();
     login.removeEventListener('click', logout);
     login.addEventListener('click', renderLogin);
 };
 const isUserLogued = async () => {
+    if (firebase.apps.length === 0) {
+        await connectFirebase();
+    }
     if (sessionStorage.getItem('RVuserID')) {
-        changeIconToLogOut();
+        UI.changeIconToLogOut();
         login.removeEventListener('click', renderLogin);
         login.addEventListener('click', logout);
         return true;
     }
     const promesa = new Promise(async (resolve, reject) => {
-        await connectFirebase();
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 renderAdminReservas();
-                changeIconToLogOut();
+                UI.changeIconToLogOut();
                 login.removeEventListener('click', renderLogin);
                 login.addEventListener('click', logout);
                 resolve(true);
             } else {
-                changeIconToLogIn();
+                UI.changeIconToLogIn();
                 login.removeEventListener('click', logout);
                 login.addEventListener('click', renderLogin);
                 resolve(false);
@@ -73,21 +59,100 @@ const renderTemplate = (template, datos, container = 'contenedor') => {
     }, 100);
 };
 
+const addMonth = () => {
+    UI.incrementMonth();
+    const fechaSelected = new Date(sessionStorage.getItem('RVfechaSelected'));
+    const month = fechaSelected.getMonth();
+    const year = fechaSelected.getFullYear();
+    renderTemplate(UI.adminReservasMonth, { month, year }, 'asrCitasContainer');
+    UI.showNameMonth(fechaSelected);
+};
+
+const subtractMonth = () => {
+    UI.decreaseMonth();
+    const fechaSelected = new Date(sessionStorage.getItem('RVfechaSelected'));
+
+    const month = fechaSelected.getMonth();
+    const year = fechaSelected.getFullYear();
+
+    renderTemplate(UI.adminReservasMonth, { month, year }, 'asrCitasContainer');
+    UI.showNameMonth(fechaSelected);
+};
+
 const selectDayOrMonth = async ({ target }) => {
+    const btnNext = document.getElementById('asrBtnNext');
+    const btnBack = document.getElementById('asrBtnBack');
+    let isBtnDaySelected = document.getElementById('asrBtnDay').dataset.selected;
+    let isBtnMonthSelected = document.getElementById('asrBtnMonth').dataset.selected;
+
     if (target.id === 'asrBtnMonth' || target.id === 'asrIconMonth') {
-        changeIconDayMonth();
-        renderTemplate(adminReservasMonth, { month: 3, year: 2020 }, 'asrCitasContainer');
+        UI.changeIconDayMonth();
+
+        btnNext.removeEventListener('click', UI.incrementDay);
+        btnNext.addEventListener('click', addMonth);
+        btnBack.removeEventListener('click', UI.decreaseDay);
+        btnBack.addEventListener('click', subtractMonth);
+
+        isBtnDaySelected = false;
+        isBtnMonthSelected = true;
+
+        const fechaSelected = new Date(sessionStorage.getItem('RVfechaSelected'));
+        const month = fechaSelected.getMonth();
+        const year = fechaSelected.getFullYear();
+        renderTemplate(UI.adminReservasMonth, { month, year }, 'asrCitasContainer');
+        UI.showNameMonth(fechaSelected);
     } else if (target.id === 'asrBtnDay' || target.id === 'asrIconDay') {
-        changeIconMonthToDay();
+        UI.changeIconMonthToDay();
+
+        btnNext.removeEventListener('click', addMonth);
+        btnNext.addEventListener('click', UI.incrementDay);
+        btnBack.removeEventListener('click', subtractMonth);
+        btnBack.addEventListener('click', UI.decreaseDay);
+
+        isBtnDaySelected = true;
+        isBtnMonthSelected = false;
         const reservas = await getReservas();
-        renderTemplate(adminReservasDay, reservas, 'asrCitasContainer');
+        renderTemplate(UI.adminReservasDay, reservas, 'asrCitasContainer');
+        const asrFecha = document.getElementById('asrFechaDDMMYYYY');
+        const asrNombreDia = document.getElementById('asrFechaNombreDia');
+        const fechaSelected = new Date(sessionStorage.getItem('RVfechaSelected'));
+
+        asrFecha.textContent = fechaSelected.toLocaleDateString('es-ES', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+        });
+        asrNombreDia.textContent = fechaSelected.toLocaleDateString('es-ES', { weekday: 'long' });
     }
 };
 
-const renderAdminReservas = async (fecha) => {
-    const reservas = await getReservas(fecha);
+const renderAdminReservas = async (_fecha) => {
+    const reservas = await getReservas(_fecha);
+    let fechaSelected = sessionStorage.getItem('RVfechaSelected');
+    if (fechaSelected === null) {
+        fechaSelected = new Date();
+        sessionStorage.setItem('RVfechaSelected', fechaSelected);
+    } else fechaSelected = new Date(fechaSelected);
 
-    renderTemplate(adminShowReservasTemplate, reservas);
+    renderTemplate(UI.adminShowReservasTemplate, reservas);
+
+    const isBtnDaySelected = document.getElementById('asrBtnDay').dataset.selected;
+    const isBtnMonthSelected = document.getElementById('asrBtnMonth').dataset.selected;
+
+    const btnNext = document.getElementById('asrBtnNext');
+    const btnBack = document.getElementById('asrBtnBack');
+    btnNext.addEventListener('click', UI.incrementDay);
+    btnBack.addEventListener('click', UI.decreaseDay);
+
+    const asrFecha = document.getElementById('asrFechaDDMMYYYY');
+    const asrNombreDia = document.getElementById('asrFechaNombreDia');
+    asrFecha.textContent = fechaSelected.toLocaleDateString('es-ES', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    });
+    asrNombreDia.textContent = fechaSelected.toLocaleDateString('es-ES', { weekday: 'long' });
+
     const btnSelectDayMonth = document.getElementById('asr_btnSelectDayMonth');
     btnSelectDayMonth.addEventListener('click', selectDayOrMonth);
 };
@@ -95,7 +160,7 @@ const renderAdminReservas = async (fecha) => {
 const renderHome = async () => {
     if (await isUserLogued()) renderAdminReservas();
     else {
-        renderTemplate(homeTemplate);
+        renderTemplate(UI.homeTemplate);
         const register = document.getElementById('btnRegister');
         register.addEventListener('click', renderRegister);
     }
@@ -104,7 +169,7 @@ const renderHome = async () => {
 const renderLogin = async (e) => {
     if (await isUserLogued()) logout();
     else {
-        renderTemplate(loginTemplate);
+        renderTemplate(UI.loginTemplate);
         e.preventDefault();
         verifyLoginUser();
         const btnLogin = document.getElementById('btnLogin');
@@ -114,7 +179,7 @@ const renderLogin = async (e) => {
 
 const renderRegister = (e) => {
     e.preventDefault();
-    renderTemplate(userRegistrationTemplate);
+    renderTemplate(UI.userRegistrationTemplate);
 
     const btnSiguiente1 = document.getElementById('btnFormulario__siguiente1');
     const btnSiguiente2 = document.getElementById('btnFormulario__siguiente2');
@@ -122,18 +187,14 @@ const renderRegister = (e) => {
     const btnVolverTo1 = document.getElementById('btnFormulario__volverTo1');
     const btnVolverTo2 = document.getElementById('btnFormulario__volverTo2');
 
-    btnSiguiente1.addEventListener('click', registerGoToStep2);
-    btnSiguiente2.addEventListener('click', registerGoToStep3);
+    btnSiguiente1.addEventListener('click', UI.registerGoToStep2);
+    btnSiguiente2.addEventListener('click', UI.registerGoToStep3);
 
-    btnVolverTo2.addEventListener('click', registerBackToStep2);
-    btnVolverTo1.addEventListener('click', registerBackToStep1);
+    btnVolverTo2.addEventListener('click', UI.registerBackToStep2);
+    btnVolverTo1.addEventListener('click', UI.registerBackToStep1);
 
     const btnCreateAccount = document.getElementById('btnCreateAccount');
     btnCreateAccount.addEventListener('click', verifyUserBySMS);
-
-    document.querySelector('form').addEventListener('keydown', (e) => {
-        if (e.which == 9) e.preventDefault();
-    });
 };
 
 renderHome();
--- src/public/js/userRegistration.js
+++ src/public/js/userRegistration.js
@@ -15,51 +15,56 @@ const getDataFromInputs = () => {
 };
 
 const verifyUserBySMS = async (e) => {
-    e.preventDefault();
-    const userData = getDataFromInputs();
-    document.getElementById('registro').style.display = 'none';
-    if (firebase.apps.length === 0) {
-        await connectFirebase();
-    }
-    let ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+    const formRegistro = document.getElementById('formRegistro');
+    formRegistro.checkValidity();
+    if (formRegistro.checkValidity()) {
+        e.preventDefault();
+        const userData = getDataFromInputs();
+        document.getElementById('registro').style.display = 'none';
+        if (firebase.apps.length === 0) {
+            await connectFirebase();
+        }
+        let ui =
+            firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
 
-    var uiConfig = {
-        callbacks: {
-            signInSuccessWithAuthResult: (authResult) => {
-                const userDataWithPhone = {
-                    ...userData,
-                    userPhone: authResult.user.phoneNumber,
-                };
-                fetch(`${url}addUser`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(userDataWithPhone),
-                });
-                return false;
-            },
-        },
-        signInFlow: 'popup',
-        signInSuccessUrl: url,
-        signInOptions: [
-            {
-                provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-                recaptchaParameters: {
-                    type: 'image', // 'audio'
-                    size: 'invisible', // 'invisible' or 'compact'
-                    badge: 'bottomleft', //' bottomright' or 'inline' applies to invisible.
+        var uiConfig = {
+            callbacks: {
+                signInSuccessWithAuthResult: (authResult) => {
+                    const userDataWithPhone = {
+                        ...userData,
+                        userPhone: authResult.user.phoneNumber,
+                    };
+                    fetch(`${url}addUser`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(userDataWithPhone),
+                    });
+                    return false;
                 },
-                defaultCountry: 'ES',
-                displayName: userData.userName,
             },
-        ],
-        // Terms of service url.
-        tosUrl: '',
-        privacyPolicyUrl: '',
-    };
-    // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
+            signInFlow: 'popup',
+            signInSuccessUrl: url,
+            signInOptions: [
+                {
+                    provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                    recaptchaParameters: {
+                        type: 'image', // 'audio'
+                        size: 'invisible', // 'invisible' or 'compact'
+                        badge: 'bottomleft', //' bottomright' or 'inline' applies to invisible.
+                    },
+                    defaultCountry: 'ES',
+                    displayName: userData.userName,
+                },
+            ],
+            // Terms of service url.
+            tosUrl: '',
+            privacyPolicyUrl: '',
+        };
+        // The start method will wait until the DOM is loaded.
+        ui.start('#firebaseui-auth-container', uiConfig);
+    }
 };
 
 // const btnCreateAccount = document.getElementById('btnCreateAccount');
